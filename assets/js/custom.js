82bcc4f792070e2f89dbc7a4797353998f2e0c2a
parimal <parimal.tbd@gmail.com>
2022-08-25 13:28:01 +0600

    responsive complete wih mobile

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -2,16 +2,26 @@ $(document).ready(function () {
   $(".slide").slick({
     slidesToShow: 3,
     slidesToScroll: 1,
-    autoplay: true,
+   autoplay: true,
     loop: true,
     Infinity: true,
     autoplaySpeed: 2000,
     dots: true,
+    arrows: {
+      default: false,
+    },
   });
+
+
   $(function () {
     $.scrollUp({
       scrollText: "Top",
       animation: "fade",
     });
   });
+
+
+
+  $('#nav').slicknav({
+  })
 });
